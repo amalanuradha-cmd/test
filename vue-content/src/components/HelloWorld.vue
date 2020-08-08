6556899bab5fa26d055440a30e725ee48b924841
Amal <amalanuradha01@gmail.com>
2020-08-08 18:42:02 +0530

    ;atest

---
+++ vue-content/src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template lang="html">
+
+  <section class="tasks">
+    <h1>tasks Component</h1>
+    <v-data-table
+      :headers="headers"
+      :items="tasks"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </section>
+
+</template>
+
+<script lang="js">
+  import axios from 'axios'
+  export default  {
+    name: 'tasks',
+    props: [],
+    mounted () {
+      axios
+            .get('http://localhost:8000/api/tasks')
+            .then(response => {this.tasks = response.data.data
+            
+    })
+            
+            
+    },
+    data () {
+      return {
+        info: null,
+        tasks: [],
+        headers: [
+          {
+   "text": "created_at",
+   "value": "created_at"
+  },
+  {
+    "text": "date",
+    "value": "date"
+   },
+   {
+    "text": "deleted_at",
+    "value": "deleted_at"
+   },
+   {
+    "text": "description",
+    "value": "description"
+   },
+   {
+    "text": "id",
+    "value": "id"
+   },
+   {
+    "text": "location",
+    "value": "location"
+   },
+   {
+    "text": "updated_at",
+    "value": "updated_at"
+   }
+          
+        ]
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .tasks {
+
+  }
+</style>
---
+++ vue-content/src/components/tasks.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="tasks">
+    <h1>tasks Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'tasks',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .tasks {
+
+  }
+</style>
